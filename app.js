5f14dd53ae6b04cf9ac4e1a26e62b62e0f7a0742
Адель <adel.absalyamov@yandex.ru>
2021-06-13 19:39:10 +0300

    add keydown

--- app.js
+++ app.js
@@ -17,6 +17,14 @@ downBtn.addEventListener('click', () => {
     changesSlide('down')
 })
 
+document.addEventListener('keydown', e => {
+    if(e.key === 'ArrowUp') {
+        changesSlide('up')
+    } else if(e.key === 'ArrowDown') {
+        changesSlide('down')
+    }
+})
+
 function changesSlide(direction) {
     if (direction === 'up') {
         indexSlide++
